9f09e96b1d42eba5ddb91753320f8fb0f767cc60
Erik Seyti Johansson <erikseyti@gmail.com>
2018-04-13 20:14:43 -0300

    Cadastro de Usuario
    Criado Tela de cadastro de usuarios

--- ConectorAndroidPHP/app/src/main/java/com/example/erik/conectorandroidphp/CadastroUsuario.java
+++ ConectorAndroidPHP/app/src/main/java/com/example/erik/conectorandroidphp/CadastroUsuario.java
@@ -2,6 +2,9 @@ package com.example.erik.conectorandroidphp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class CadastroUsuario extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class CadastroUsuario extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_usuario);
+    }
+    public void CadastrarUsuario(View view)
+    {
+        EditText editNome = (EditText) findViewById(R.id.editNome);
+        EditText editCidade = (EditText)findViewById(R.id.editCidade);
+        String usuarioNome = editNome.getText().toString();
+        String usuarioCidade = editCidade.getText().toString();
+
+        Toast.makeText(this,"Nome do Usuario: "+usuarioNome,Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,"Cidade do Usuario: "+usuarioCidade,Toast.LENGTH_SHORT).show();
     }
 }
