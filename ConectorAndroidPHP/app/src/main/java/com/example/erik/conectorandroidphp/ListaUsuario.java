65c71d68e4e40dd7485e134c13c02ce60168d16c
Erik Seyti Johansson <erikseyti@gmail.com>
2018-04-13 21:03:19 -0300

    Lista Usuarios #1
    Adicionado Adapter para a listview da Lista de Usuarios

--- ConectorAndroidPHP/app/src/main/java/com/example/erik/conectorandroidphp/ListaUsuario.java
+++ ConectorAndroidPHP/app/src/main/java/com/example/erik/conectorandroidphp/ListaUsuario.java
@@ -2,12 +2,32 @@ package com.example.erik.conectorandroidphp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class ListaUsuario extends AppCompatActivity {
+public class ListaUsuario extends AppCompatActivity implements AdapterView.OnItemClickListener {
+    private ListView listViewUsuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_usuario);
+        listViewUsuario = (ListView) findViewById(R.id.listUsuarios);
+
+        //lista simples de teste
+        String usuarios[] = {"Erik","Leonardo"};
+        // criando um adapter simples para preencher a lista de usuarios
+        // este adapter exibe uma lista de usuarios com apenas um dado por linha
+        ArrayAdapter<String> adapterUsuario = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,usuarios);
+        listViewUsuario.setAdapter(adapterUsuario);
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        Log.i("MainActivity",adapterView.getItemAtPosition(i).toString());
     }
 }
